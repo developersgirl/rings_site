83f455ed37173a4614da8ba9f47cfce7eea2f468
developersgirl <xusninigor17@gmail.com>
2023-01-02 19:24:54 +0500

    Add footer new folder

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,9 @@ import bx_vk from '../Footer/img/social_vk.png';
 import bx_facebook from '../Footer/img/bx_facebook.png';
 import bx_telegram from '../Footer/img/bx_telegram.png';
 import bx_etsy from '../Footer/img/bx_etsy.png';
+import mastercard from '../Footer/img/master.png';
+import tel from '../Footer/img/phone.png';
+import email from '../Footer/img/mail.png';
 
 
 const Footer = () => {
@@ -14,6 +17,7 @@ const Footer = () => {
             <div className="footer_column1">
                 <h1>Полезные ссылки</h1>
                 <hr />
+                {/* <div className="line_div"></div> */}
                 <a href="#">Доставка</a> <br />
                 <a href="#">Оплата</a> <br />
                 <a href="#">Акции</a> <br />
@@ -21,34 +25,37 @@ const Footer = () => {
             </div>
             <div className="footer_column2">
                 <h1>оплата</h1>
-                <div className="line_div"></div>
+                <hr />
+                {/* <div className="line_div"></div> */}
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper justo, nec, pellentesque.</p>
                 <div className="visa_img">
                     <img src={visa} />
-                    {/* <img src={mastercard} /> */}
+                    <img src={mastercard} />
                 </div>
             </div>
             <div className="footer_column3">
                 <h1>контакты</h1>
-                <div className="line_div"></div>
+                <hr />
+                {/* <div className="line_div"></div> */}
                 <div className="tel_number">
                     <div className="number">
-                        {/* <img src={tel} /> */}
+                        <img src={tel} />
                         <number>8 (812) 234-56-55</number>
                     </div> <br />
                     <div className="number">
-                        {/* <img src={tel} /> */}
+                        <img src={tel} />
                         <number>8 (812) 234-56-55</number>
                     </div> <br />
                     <div className="number">
-                        {/* <img src={tel} /> */}
+                        <img src={email} />
                         <number>ojjo@ojjo.ru</number>
                     </div>
                 </div>
             </div>
             <div className="footer_column4">
                 <h1>социальные сети</h1>
-                <div className="line_div"></div>
+                <hr />
+                {/* <div className="line_div"></div> */}
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper justo, nec, pellentesque.</p>
                 <div className="carbon">
                     <img src={bx_youtube} />
